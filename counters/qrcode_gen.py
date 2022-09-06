d3bc100dbf8fd47e1b97ae5276284d28ff6ee837
Darlon Vasata <darlonv@gmail.com>
2022-09-06 20:48:01 -0300

    codigo para gerar qrcode e qrcode

---
+++ counters/qrcode_gen.py
@@ -0,0 +1,16 @@
+
+#pip3 install pillow qrcode
+
+import qrcode
+
+input_data = 'https://darlonv.github.io/counters/ferias.html'
+
+qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=5)
+
+qr.add_data(input_data)
+qr.make(fit=True)
+img = qr.make_image(fill='black', back_color='white')
+img.save('qrcode_ferias.png')
